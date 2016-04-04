913a2d7f5b3bca534cfafa6ee0467e84e0ee8a89
Sabin Maharjan <sabin.mhrj@gmail.com>
2016-04-03 23:15:00 -0700

    Update sendCmdConverted.cpp

--- Sabin-Code/sendCmdConverted.cpp
+++ Sabin-Code/sendCmdConverted.cpp
@@ -1,4 +1,75 @@
 
+
+#include <unistd.h>
+#include <iostream>
+#include <exception>
+#include <bitset>
+
+#include "mraa.hpp"
+
+void setup();
+
+std::string binary(uint8_t);
+
+void get_data(uint8_t);
+
+void send_cmd(uint8_t, uint8_t*, uint8_t);
+
+mraa::Uart* dev;
+
+int main()
+{
+	setup();
+
+	get_data(100);
+
+	return EXIT_SUCCESS;
+}
+
+void send_cmd(uint8_t opcode, uint8_t* data, uint8_t data_length)
+{
+	uint8_t checksum = 0;
+
+	dev->writeStr("$M<");
+
+	char length_str[sizeof(std::string)];
+	sprintf(length_str, "%x", data_length);
+	dev->writeStr(length_str);
+	checksum ^= data_length;
+
+	char opcode_str[sizeof(std::string)];
+	sprintf(opcode_str, "%x", opcode);
+	dev->writeStr(opcode_str);
+	checksum ^= opcode;
+
+	for (int i = 0; i < data_length; i++)
+	{
+		char data_str[sizeof(std::string)];
+		sprintf(data_str, "%x", data[i]);
+		dev->writeStr(data_str);
+		checksum ^= data[i];
+	}
+
+	char checksum_str[sizeof(std::string)];
+	sprintf(checksum_str, "%x", checksum);
+
+	dev->writeStr(checksum_str);
+}
+
+
+void get_data(uint8_t cmd)
+{
+	send_cmd(cmd, 0, 0);
+
+	if(dev->dataAvailable())
+	{
+		std::cout << dev->readStr(0) << std::endl;
+	}
+}
+
+
+void setup()
+{
 	try {
 		dev = new mraa::Uart(0);
 	} catch (std::exception& e) {
@@ -28,3 +99,14 @@
 	if (dev->setFlowcontrol(false, false) != mraa::SUCCESS) {
 		std::cout << "Error setting flow control UART" << std::endl;
 	}
+}
+
+std::string binary(uint8_t value)
+{
+	std::bitset<8> x(value);
+	x.set();
+	std::string binary_value = x.to_string<char,std::string::traits_type,std::string::allocator_type>();
+
+	return binary_value;
+}
+
