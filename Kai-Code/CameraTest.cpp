6b49026471ba68ffa1347c35aa8eeeed8e42c229
Kekahuna <kekahuna@pdx.edu>
2016-04-06 17:14:03 -0700

    Update CameraTest.cpp

--- Kai-Code/CameraTest.cpp
+++ Kai-Code/CameraTest.cpp
@@ -1,3 +1,8 @@
+// This code is my sample code that I am uploading to the Edison to prove that my OpenCV installation is functioning properly. 
+// Currently, when running 'make CameraTest' while in the opencv-3.1.0/samples directory, I get an error that states,
+// fatal error: opencv2/opencv.hpp: No such file or directory #include <opencv2/opencv.hpp>"
+// That .hpp file does indeed exist in opencv-3.1.0/include/opencv2/opencv.hpp
+
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
