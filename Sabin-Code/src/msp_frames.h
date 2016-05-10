b11f4a9964dff531e644f584d1ffef8a8d6b3c8a
Sabin Maharjan <sabin.mhrj@gmail.com>
2016-05-10 00:02:05 -0700

    added gdb option in makefile

--- Sabin-Code/src/msp_frames.h
+++ Sabin-Code/src/msp_frames.h
@@ -13,6 +13,12 @@ struct attitude_frame {
 	float ang_x; // 16 bit Range [-1800;1800] (unit: 1/10 degree)
 	float ang_y; // 16 bit Range [-900;900] (unit: 1/10 degree)
 	float heading; // 16 bit [-180;180]
+
+	attitude_frame():
+		ang_x(0.00),
+		ang_y(0.00),
+		heading(0)
+		{}
 };
 
 struct altitude_frame {
@@ -25,6 +31,13 @@ struct set_raw_rc_frame {
 	uint16_t pitch; // 16 bit Range [1000;2000]
 	uint16_t yaw; // 16 bit Range [1000;2000]
 	uint16_t throttle; // 16 bit Range [1000;2000]
+
+	set_raw_rc_frame() :
+		roll(0),
+		pitch(0),
+		yaw(0),
+		throttle(0)
+		{}
 };
 
 #endif
